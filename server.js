17e1dbaea2889c62b13bacd8725adc731aaa10d2
edStarr <64718604+edStarr@users.noreply.github.com>
2020-05-14 14:37:50 -0700

    JsonPackageFile

---
+++ server.js
@@ -0,0 +1,41 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: '.env' })
+}
+
+const express = require ("express")
+const app = express()
+const expressLayouts = require ("express-ejs-layouts")
+
+
+const indexRouter = require("./routes/index")
+
+
+
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
+app.set("layout", "layouts/layout")
+app.use(expressLayouts)
+app.use(express.static("public"))
+
+
+const mongoose = require ("mongoose")
+
+const mongoURI = require ("./config/keys").mongoURI
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex : true,
+    useUnifiedTopology : true
+})
+
+
+
+const db = mongoose.connection
+db.on("error", error => console.error(error))
+db.once("open", () => console.log("connection established..."))
+
+
+app.use("/", indexRouter)
+
+
+app.listen(process.env.PORT || 3000)
